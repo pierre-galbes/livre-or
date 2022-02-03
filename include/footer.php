1051b70ff4feb00b4d0133c96e3fb192b9928b5a
pierre-galbes <pierre.galbes@laplateforme.io>
2022-02-03 16:58:43 +0100

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Untitled</title>
-   
-    
+
+
     <link rel="stylesheet" href="../css/footer.css">
 </head>
 
@@ -15,22 +15,18 @@
         <footer>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-6 col-md-3 item">
-                        <h3>Site</h3>
-                        <ul>
-                            <li><a href="#">Accueil</a></li>
-                            <li><a href="#">Livre d'or</a></li>
-                            
-                        </ul>
-                    </div>
 
-                    <div class="col-md-6 item text">
-                        <h3</h3>
-                        <p></p>
-                                        </div>
-                                        <a href='https://github.com/pierre-galbes/livre-or.git'>Github.</a>
+                    <h3>Site</h3>
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Livre d'or</a></li>
+
+                    </ul>
+
                 </div>
-                
+                <a href='https://github.com/pierre-galbes/livre-or.git'>Github.</a>
+
+
             </div>
         </footer>
     </div>
